57e42f7f1f0f53a0f1e884885fbf9d92f9bec081
Lior Kaplun <liorkaplun@campus.technion.ac.il>
2023-04-30 16:04:14 +0300

    added comments to bp.cpp and recompiled

--- HW1/bp.cpp
+++ HW1/bp.cpp
@@ -18,19 +18,21 @@ enum FSM_STATES{
 
 SIM_stats stats;
 
-
+// init history to zeros/falses
 void initHist(bool* history, unsigned historySize){
 	for(unsigned i = 0; i < historySize; i++){
 		history[i] = false;
 	}
 }
 
+//init all fsm table slots to the default fsm state
 void initFsmTable(FSM_STATES* fsmTable, unsigned fsmState, unsigned historySize){
 	for(int i = 0; i < pow(2,historySize); i++){
 		fsmTable[i] = FSM_STATES(fsmState);
 	}
 }
 
+// A class that represent a row in the btb table
 class BP_ENTRY{
 public:
 	int  tag;
@@ -68,7 +70,7 @@ public:
 	}
 };
 
-
+// A class that represents the branch predictor - btb table, GHR and global fsm table.
 
 class BP{
 public:
@@ -156,6 +158,7 @@ public:
 	}
 };
 
+// the branch predictor instance
 BP* branchPredictor = new BP(0,0,0,0,false,false,0);
 
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
@@ -163,6 +166,7 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 	stats.br_num = 0;
 	stats.flush_num = 0;
 	stats.size = 0;
+	// create a branch predictor with the wanted configuration and assign it to the global bp.
 	BP* newBranchPredictor = new BP(btbSize, historySize, tagSize, fsmState, isGlobalHist, isGlobalTable, Shared);
 	if(newBranchPredictor == NULL){
 		return -1;
@@ -171,6 +175,7 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 	return 0;
 }
 
+// based on the wanted share config it returns the calculated val to be xored with the history reg
 int shareHandler(int shared, unsigned historySize, uint32_t pc){
 	int mask = 1;
 	for(unsigned i = 1; i < historySize; i++){
@@ -187,6 +192,7 @@ int shareHandler(int shared, unsigned historySize, uint32_t pc){
 	}
 }
 
+//returns an index to the fsmtable based on the history reg and the desired configuration
 int tableIdxHandler(bool* histArr, unsigned historySize, bool isGlobalTable, int shared, uint32_t pc){
 	int idx = 0;
 	for(unsigned i = 0; i < historySize - 1; i++){
@@ -202,6 +208,7 @@ int tableIdxHandler(bool* histArr, unsigned historySize, bool isGlobalTable, int
 	return idx;
 }
 
+//maps the 4 fsm states to taken/not taken decisions
 bool fsmStateMapping(FSM_STATES state){
 	if(state == ST || state == WT){
 		return true;
@@ -211,6 +218,7 @@ bool fsmStateMapping(FSM_STATES state){
 	}
 }
 
+//calcs the btb table index from the pc
 int pc2btbIndex(uint32_t pc, int btbIdxSize){
 	int mask = 1;
 	for(int i = 1; i < btbIdxSize; i++){
@@ -219,6 +227,7 @@ int pc2btbIndex(uint32_t pc, int btbIdxSize){
 	return (pc >> 2) & mask;
 }
 
+//calcs the tag from the pc
 int pc2tag(uint32_t pc, int btbIdxSize, unsigned tagSize){
 	int mask = 1;
 	for(unsigned i = 1; i < tagSize; i++){
@@ -230,10 +239,15 @@ int pc2tag(uint32_t pc, int btbIdxSize, unsigned tagSize){
 bool BP_predict(uint32_t pc, uint32_t *dst){
 	int btbIndex = pc2btbIndex(pc, branchPredictor->_btbIndexSize);
 	int tag = pc2tag(pc, branchPredictor->_btbIndexSize, branchPredictor->_tagSize);
+	//taking the matching entry based on the btb index that was calculated from pc
 	BP_ENTRY* entry = branchPredictor->_btb[btbIndex];
+	//taking the right history based on config
 	bool* history = (branchPredictor->_isGlobalHist) ? branchPredictor->_gHistory : entry->history;
+	//calc the fsm table idx based on history and config
 	int tableIdx = tableIdxHandler(history,branchPredictor->_historySize, branchPredictor->_isGlobalTable, branchPredictor->_shared, pc);
+	//taking the right fsm table based on config
 	FSM_STATES* fsmTable = (branchPredictor->_isGlobalTable) ? branchPredictor->_gFsmTable : entry->fsmTable;
+	//if the pc is already known ad branch and the prediction is "taken" then predicted target is set if not the next pc is set as a target
 	if(entry->isBranch == true && entry->tag == tag && fsmStateMapping(fsmTable[tableIdx]) == true){
 		*dst = entry->target;
 		return true;
@@ -244,6 +258,7 @@ bool BP_predict(uint32_t pc, uint32_t *dst){
 	}
 }
 
+//update the desired fsm in the fsm table based on correctness of the prediction
 void updateFSM(FSM_STATES* fsmTable, int tableIdx, bool isCorrect){
 	if(fsmTable[tableIdx] == ST){
 		fsmTable[tableIdx] = (isCorrect) ? ST : WT;
@@ -259,6 +274,7 @@ void updateFSM(FSM_STATES* fsmTable, int tableIdx, bool isCorrect){
 	}
 }
 
+//update the history based on the real decisions
 void updateHist(bool* history, unsigned historySize, bool taken){
 	for(unsigned i = 1 ; i < historySize ; i++){
 		history[i-1] = history[i];
@@ -270,9 +286,13 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 	stats.br_num++;
 	int btbIndex = pc2btbIndex(pc, branchPredictor->_btbIndexSize);
 	int tag = pc2tag(pc, branchPredictor->_btbIndexSize, branchPredictor->_tagSize);
+	//taking the matching entry based on the btb index that was calculated from pc
 	BP_ENTRY* entry = branchPredictor->_btb[btbIndex];
+	//taking the right history based on config
 	bool* history = (branchPredictor->_isGlobalHist) ? branchPredictor->_gHistory : entry->history;
+	//taking the right fsm table based on config
 	FSM_STATES* fsmTable = (branchPredictor->_isGlobalTable) ? branchPredictor->_gFsmTable : entry->fsmTable;
+	//if the pc is not known as branch or pc with the same entry index but different tag then update the new vals to btb
 	if(entry->isBranch != true || entry->tag != tag){
 		entry->isBranch = true;
 		entry->tag = tag;
@@ -284,19 +304,23 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 		}
 	}
 	entry->target = targetPc;
+	//get the fsm table index
 	int tableIdx = tableIdxHandler(history,branchPredictor->_historySize, branchPredictor->_isGlobalTable, branchPredictor->_shared, pc);
+	// update the right fsm
 	if(fsmStateMapping(fsmTable[tableIdx]) != taken){
 		updateFSM(fsmTable,tableIdx,false);
 	}
 	else{
 		updateFSM(fsmTable,tableIdx,true);
 	}
+	// decied if flush or not and count
 	if(taken && targetPc != pred_dst){
 		stats.flush_num++;
 	}
 	else if(!taken && pred_dst != (pc + 4)){
 		stats.flush_num++;
 	}
+	// update the history with the actual decision
 	updateHist(history, branchPredictor->_historySize, taken);
 }
 
